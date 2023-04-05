dcfdb0512ade75c03b8c979429e4bb8a6a7cc9c2
danny <1058952@hr.nl>
2023-04-05 11:31:45 +0200

    PHP json list gemaakt + java toegevoegd

--- JS/danny.js
+++ JS/danny.js
@@ -1,30 +1,37 @@
-// get modal content
-let modal = document.getElementById('simpleModal');
-//get open modal button
-let modalBtn = document.getElementById('modalBtn');
-//get close button
-let closeBtn =document.getElementsByClassName('closeBtn')[0];
+window.addEventListener("load",init);
+let modal;
+let modalBtn;
+let closeBtn;
 
+
+function init() {
+// get modal content
+    modal = document.getElementById('simpleModal');
+//get open modal button
+    modalBtn = document.getElementById('modalBtn');
+//get close button
+    closeBtn = document.getElementsByClassName('closeBtn')[0];
+}
 // listen for open click
-modalBtn.addEventListener('click', openModal);
+    modalBtn.addEventListener('click', openModal);
 //listen for close click
-closeBtn.addEventListener('click', closeModal);
+    closeBtn.addEventListener('click', closeModal);
 //listen for outside click
-window.addEventListener('click', clickOutside);
+    window.addEventListener('click', clickOutside);
 
 //function to open modal
-function openModal(){
-    modal.style.display = 'block';
-}
+    function openModal() {
+        modal.style.display = 'block';
+    }
 
 //function to close modal
-function closeModal(){
-    modal.style.display = 'none';
-}
-
-//function to close modal if outside click
-function clickOutside(e){
-    if(e.target == modal) {
+    function closeModal() {
         modal.style.display = 'none';
     }
-}
+
+//function to close modal if outside click
+    function clickOutside(e) {
+        if (e.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
